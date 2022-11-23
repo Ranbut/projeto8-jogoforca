b1a74b1b1f120b29a0603e7c31f13e911b98687f
Ranbut <Ranbut_Beater@protonmail.com>
2022-11-23 13:05:31 -0300

    Atualizado Layout

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,24 @@
 import Chute from './Chute';
 import Jogo from './Jogo';
 import Letras from './Letras';
-import Palavras from '../Palavras';
 
 const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+let jogando = false;
+let erros = 0;
 
 function App() {
   return(
     <>
         <Jogo />
         <Letras />
+        <div className="containerLetras">
+            {alfabeto.map(letra => <button key={letra} className={(jogando) ? 'letraSelecao selecionavel' : 'letraSelecao'}>{letra.toUpperCase()}</button>)}
+        </div>
         <Chute />
     </>
   );
 }
 
 export default App;
-export {alfabeto};
+export {erros};
--- src/Components/Letras.js
+++ src/Components/Letras.js
@@ -1,12 +1,9 @@
-import App, { alfabeto } from "./App";
+import { alfabeto } from "./App";
 
 function Letras () {
     return(
     <>
         <h4 className="palavra">_ _ _ _ _ _ _ _ _ _</h4>
-        <div className="containerLetras">
-            {alfabeto.map(letra => <button key={letra} className="letraSelecao">{letra.toUpperCase()}</button>)}
-        </div>
     </>
       );
 }
