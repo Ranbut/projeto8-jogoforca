b5d0a135cd5ab24c4fbdc7316e47704fc8993117
Ranbut <Ranbut_Beater@protonmail.com>
2022-11-23 13:22:04 -0300

    Atualizado Layout

--- src/Components/Jogo.js
+++ src/Components/Jogo.js
@@ -1,7 +1,9 @@
+import { erros } from "./App";
+
 function Jogo () {
     return(
     <>
-        <img className="forca" src="./assets/forca0.png" alt="forca" />
+        <img className="forca" src={`./assets/forca${erros}.png`} alt="forca" />
         <button className="botaoPalavra">Escolher Palavra</button>
     </>
     );
